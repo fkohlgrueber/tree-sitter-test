810a14a177e3c537c434f4d99ee2aef6cd0a3f3f
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2019-08-08 11:10:20 +0200

    update build.rs

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ extern crate cc;
 use std::path::PathBuf;
 
 fn main() {
-    let tree_sitter_rust: PathBuf = std::fs::canonicalize::<PathBuf>(["..", "tree-sitter-rust", "src"].iter().collect()).unwrap();
+    let tree_sitter_rust: PathBuf = std::fs::canonicalize::<PathBuf>([".", "tree-sitter-rust", "src"].iter().collect()).unwrap();
     
     cc::Build::new()
         .include(&tree_sitter_rust)
